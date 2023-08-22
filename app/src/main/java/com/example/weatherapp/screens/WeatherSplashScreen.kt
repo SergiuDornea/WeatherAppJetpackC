6d30d660976876cd716b0c9cf1b0b1f281b5e124
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2023-08-22 19:15:22 +0300

    1.1  SplashScreen UI ☀

--- app/src/main/java/com/example/weatherapp/screens/WeatherSplashScreen.kt
+++ app/src/main/java/com/example/weatherapp/screens/WeatherSplashScreen.kt
@@ -1,37 +1,61 @@
 package com.example.weatherapp.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.weatherapp.R
 
 @Preview(showBackground = true)
 @Composable
 fun WeatherSplashScreen(navController: NavController = rememberNavController()){
     Surface(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .padding(15.dp)
+            .size(330.dp),
+        color = Color.White
     ) {
         Column(
             modifier = Modifier.border(
                 5.dp,
                 brush = Brush.linearGradient(listOf( Color.Blue, Color.Cyan)),
                 shape = CircleShape
-            )
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ){
+            Image(painter = painterResource(
+                id =  R.drawable.splash_weather),
+                contentDescription = "Sun icon",
 
-            Text(text = "How's the weather?")
+                )
+            Text(
+                text = "How's the weather?",
+                color = Color.Cyan,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+
+                )
 
         }
 
